167b84d5ab77dfedb86d040de2d3ad73b8a7ede4
ypoolz <snt.biel15@gmail.com>
2022-09-14 18:47:18 -0300

    Up

---
+++ Exercicio_04.py
@@ -0,0 +1,5 @@
+num = int(input("Digite o último número a imprimir:"))
+x = 1
+while x <= num:
+    print(x)
+    x = x + 2
---
+++ Exercicio_09.py
@@ -0,0 +1,9 @@
+n = 0
+
+while n < 4:
+    nome = input('Digite seu nome: ')
+    idade = int(input('Digite sua idade: '))
+    sexo = input('Digite seu sexo: ')
+n = n + 1
+
+# while n > 5 :
