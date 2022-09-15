7c404ccf095d2d5c1a5b332f345bc88a78b1f1f6
Gabriel Pereira <102925071+ypoolz@users.noreply.github.com>
2022-09-15 19:38:03 -0300

    Done

---
+++ Exercicio_22.py
@@ -0,0 +1,10 @@
+# Faça uma função chamada area que calcula de uma circunferência. Utilize o valor de π fornecido pelo módulo math.
+
+def area(n):
+    return area
+
+area = float(input('Entre com o valor do raio, para obter a circuferência do círculo: '))
+
+circunferencia = 2 * 3.14 * area
+
+print("A circuferência do círculo: {:.2f}".format(circunferencia))
